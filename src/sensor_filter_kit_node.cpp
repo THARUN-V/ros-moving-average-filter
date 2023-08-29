a522c560cd2717c1823ccd1bd11a7ccaa99f290c
THARUN_V <tharunv973@gmail.com>
2023-08-29 09:35:27 +0530

    modified for sensro_msgs/Imu

--- src/sensor_filter_kit_node.cpp
+++ src/sensor_filter_kit_node.cpp
@@ -1,6 +1,5 @@
 #include "sensor_filter_kit/sensor_filter_kit_lib.h"
-#include "gy_88_interface/gy_88_lib.h"
-#include "gy_88_interface/Gy88Data.h"
+#include "sensor_msgs/Imu.h"
 #include "sensor_filter_kit/SensorKitData.h"
 #include "ros/ros.h"
 #include "iostream"
@@ -23,37 +22,37 @@ struct imu_data
 
 imu_data imu_data;
 
-void imu_data_callback(const gy_88_interface::Gy88Data::ConstPtr& imu_msg)
+void imu_data_callback(const sensor_msgs::Imu::ConstPtr& imu_msg)
 {
-  imu_data.accel_x = imu_msg->accel_x;
-  imu_data.accel_y = imu_msg->accel_y;
-  imu_data.accel_z = imu_msg->accel_z;
+  imu_data.accel_x = imu_msg->linear_acceleration.x;
+  imu_data.accel_y = imu_msg->linear_acceleration.y;
+  imu_data.accel_z = imu_msg->linear_acceleration.z;
 
-  imu_data.gyro_x = imu_msg->gyro_x;
-  imu_data.gyro_y = imu_msg->gyro_y;
-  imu_data.gyro_z = imu_msg->gyro_z;
+  imu_data.gyro_x = imu_msg->angular_velocity.x;
+  imu_data.gyro_y = imu_msg->angular_velocity.y;
+  imu_data.gyro_z = imu_msg->angular_velocity.z;
 }
 
 int main(int argc, char **argv)
 {
-  int publishing_freq= 0;
-  if(argc < 2)
-  {
-    ROS_ERROR("Missing param! Try: <publishing_freq>.");
-    return 0;
-  }
-  if(atoi(argv[1]) != 0)
-  {
-    publishing_freq = atoi(argv[1]);
-    ROS_INFO_STREAM("Publishing freq. set to: " << publishing_freq);
-  }
-  else
-  {
-    ROS_ERROR("You must pass a recording freq. above 0, quitting.");
-    return 0;
-  }
+  // int publishing_freq= 0;
+  // if(argc < 2)
+  // {
+  //   ROS_ERROR("Missing param! Try: <publishing_freq>.");
+  //   return 0;
+  // }
+  // if(atoi(argv[1]) != 0)
+  // {
+  //   publishing_freq = atoi(argv[1]);
+  //   ROS_INFO_STREAM("Publishing freq. set to: " << publishing_freq);
+  // }
+  // else
+  // {
+  //   ROS_ERROR("You must pass a recording freq. above 0, quitting.");
+  //   return 0;
+  // }
 
-  const uint window_size = 100;
+  const uint window_size = 5;
   const uint SENSOR_NUMBER = 6;
   uint sensors[SENSOR_NUMBER] = {ACCEL_X, ACCEL_Y, ACCEL_Z, GYRO_X, GYRO_Y, GYRO_Z};
   float sensor_readings[SENSOR_NUMBER];
@@ -64,11 +63,12 @@ int main(int argc, char **argv)
 
   ros::init(argc, argv, "sensor_filter_kit_node");
   ros::NodeHandle n;
-  ros::Subscriber imu_sub = n.subscribe("gy88_data", 1000, imu_data_callback);
-  ros::Publisher publisher = n.advertise<sensor_filter_kit::SensorKitData>("sensor_kit_data", 1000);
-  ros::Rate loop_rate(publishing_freq);
+  ros::Subscriber imu_sub = n.subscribe("/imu0", 1000, imu_data_callback);
+  ros::Publisher publisher = n.advertise<sensor_msgs::Imu>("/Imu/Filtered", 1000);
+  // ros::Rate loop_rate(publishing_freq);
   
-  sensor_filter_kit::SensorKitData sensor_kit_data;
+  // sensor_filter_kit::SensorKitData sensor_kit_data;
+  sensor_msgs::Imu imu_filtered;
 
   std::vector<double> features;
   std::cout << std::fixed;
@@ -89,17 +89,19 @@ int main(int argc, char **argv)
 
     features = filter_kit.get_features();
     
-    sensor_kit_data.filtered_accel_x = features.at(0);
-    sensor_kit_data.filtered_accel_y = features.at(1);
-    sensor_kit_data.filtered_accel_z = features.at(2);
-    sensor_kit_data.filtered_gyro_x = features.at(3);
-    sensor_kit_data.filtered_gyro_y = features.at(4);
-    sensor_kit_data.filtered_gyro_z = features.at(5);
+    imu_filtered.linear_acceleration.x = features.at(0);
+    imu_filtered.linear_acceleration.y = features.at(1);
+    imu_filtered.linear_acceleration.z = features.at(2);
+    imu_filtered.angular_velocity.x = features.at(3);
+    imu_filtered.angular_velocity.y = features.at(4);
+    imu_filtered.angular_velocity.z = features.at(5);
   
-    sensor_kit_data.timestamp = get_millis_since_epoch();
+    // sensor_kit_data.timestamp = get_millis_since_epoch();
+    imu_filtered.header.stamp = ros::Time::now();
+    imu_filtered.header.frame_id = "imu_filtered";
     
-    publisher.publish(sensor_kit_data);
+    publisher.publish(imu_filtered);
     ros::spinOnce();
-    loop_rate.sleep();
+    // loop_rate.sleep();
   }
 }
